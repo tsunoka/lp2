a27dc4040a5bb684a0acbab390df12fa4068acbf
Tsunoka(Hosoya).Shiori <petit0426@icloud.com>
2024-09-13 12:14:30 +0900

    スムーススクロール実装

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,3 +72,17 @@ $('.qa__accordion-title').on('click', function (e) {
     // .accordion-contentを表示・非表示
     content.slideToggle();
 });
+
+//スムーススクロール
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        var speed = 500;
+        $("html, body").animate({
+            scrollTop: position
+        }, speed, "swing");
+        return false;
+    });
+});
